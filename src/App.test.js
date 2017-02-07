0e861ed6ba924390481cd1a4e808925efe88cf0b
Mariusz Błaszczak <mariusz.blaszczak@gmail.com>
2017-02-07 17:55:16 +0100

    add enzyme for testing and example specs

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Game from './App';
+import { mount } from 'enzyme';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Game />, div);
 });
+
+it('sum properly', () => {
+  expect(2+2).toBe(4);
+});
+
+it('has 3 board rows', () => {
+  const wrapper = mount(<Game />);
+  
+  expect(wrapper.find('.board-row')).to.have.length(3)
+});
+
+
